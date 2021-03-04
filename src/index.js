c34c2412794dd83391424e4d222440f97db4a45f
Akisole <drive_98@mail.ru>
2021-03-04 20:05:31 +0300

    fix: implement 'towel sort' algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  if(matrix != undefined && matrix != "") {
+    for(let i=0; i<matrix.length; i++){
+      if((i+1)%2 != 0){
+        res.push(matrix[i]);
+      }
+      else {
+        res.push(matrix[i].reverse());
+      }
+    }
+    res = res.join().split(",");
+  }
+
+  return res;
 }
